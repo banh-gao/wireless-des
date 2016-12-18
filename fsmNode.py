f81f62a67853ed357406b4ca8246312fe145e849
Daniel Zozin <zdenial@gmx.com>
2016-12-18 19:46:19 +0100

    Split non-node operation specific methods to base node class

---
+++ fsmNode.py
@@ -0,0 +1,184 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2016 Daniel Zozin <zdenial@gmx.com>
+
+from module import Module
+from distribution import Distribution
+from packet import Packet
+from event import Event
+from events import Events
+
+
+class FSMNode(Module):
+    """
+    Implement a Finite State Machine initialized with a transition table that
+    maps pairs (State, Event) to an action and an event transition.
+
+    For internal state S0 and event E0 there is a transition function T such
+    that T:(S0, E0) -> S1 changes the internal state to S1.
+    To avoid an anti-pattern the current state is not accessible from within
+    a transition function.
+    """
+
+    # transmission speed parameter (bits per second)
+    DATARATE = "datarate"
+    # queue size
+    QUEUE = "queue"
+    # inter-arrival distribution (seconds)
+    INTERARRIVAL = "interarrival"
+    # packet size distribution (bytes)
+    SIZE = "size"
+    # processing time distribution (seconds)
+    PROC_TIME = "processing"
+    # max packet size (bytes)
+    MAXSIZE = "maxsize"
+
+    STAY = -1
+
+    def __init__(self, config, channel, x, y):
+        """
+        :param initialState: The state the FSM has to start from
+        :param transitions: A dictionary that maps pairs (State, Event) to a
+        transition function. The transition function is defined as t(E) -> E
+        where E is the event to handle.
+        The FSM will move to the state returned by the function.
+        If the function returns None, the FSM remains in the same state.
+        """
+        Module.__init__(self)
+
+        # load configuration parameters
+        self.datarate = config.get_param(FSMNode.DATARATE)
+        self.queue_size = config.get_param(FSMNode.QUEUE)
+        self.interarrival = Distribution(config.get_param(FSMNode.INTERARRIVAL))
+        self.size = Distribution(config.get_param(FSMNode.SIZE))
+        self.proc_time = Distribution(config.get_param(FSMNode.PROC_TIME))
+        self.maxsize = config.get_param(FSMNode.MAXSIZE)
+
+        # save position
+        self.x = x
+        self.y = y
+
+        # save channel
+        self.channel = channel
+
+        # queue of packets to be sent
+        self.queue = []
+
+    def set_transitions(self, initialState, transitions):
+        self.state = initialState
+        self.transitions = transitions
+
+    def initialize(self):
+        """
+        Initialization. Starts node operation by scheduling the first packet
+        """
+        self.schedule_next_arrival()
+
+    def schedule_next_arrival(self):
+        """
+        Schedules a new arrival event
+        """
+        # extract random value for next arrival
+        arrival = self.interarrival.get_value()
+
+        # draw packet size from the distribution
+        packet_size = self.size.get_value()
+
+        # generate an event setting this node as destination
+        event = Event(self.sim.get_time() + arrival, Events.PACKET_ARRIVAL,
+                      self, self, packet_size)
+        self.sim.schedule_event(event)
+
+    def handle_event(self, event):
+        """
+        Handles the given event with a mapped transition function.
+        If there is no mapping an exeception will be raised.
+        :param event: the event to handle
+        """
+
+        # Log current state
+        self.logger.log_state(self, self.state)
+
+        # On arrival always try to enqueue and schedule next packet arrival
+        if(event.get_type() == Events.PACKET_ARRIVAL):
+            self.schedule_next_arrival()
+            # If enqueue fails don't notify the node and remain in the
+            # same state
+            if(not self.enqueue_arrived(event)):
+                return
+
+        key = (self.state, event.get_type())
+
+        if key not in self.transitions.keys():
+            raise AssertionError("Unhandled event %s in state %s" %
+                                 (event.get_type(), self.state))
+
+        action = self.transitions[key]
+
+        nextState = action(event)
+
+        if(nextState is None):
+            raise AssertionError("Unknown next state for transition"
+                                 + " function %s" % action.__name__)
+
+        if(nextState is not FSMNode.STAY):
+            self.state = nextState
+
+    def enqueue_arrived(self, event):
+        packet_size = event.get_obj()
+        self.logger.log_arrival(self, packet_size)
+
+        if self.queue_size == 0 or len(self.queue) < self.queue_size:
+            # if queue size is infinite or there is still space
+            self.queue.append(packet_size)
+            self.logger.log_queue_length(self, len(self.queue))
+            return True
+        else:
+            # if there is no space left, we drop the packet and log
+            self.logger.log_queue_drop(self, packet_size)
+            return False
+
+    def stay(self, event):
+        return self.STAY
+
+    def transmit(self):
+        assert(len(self.queue) > 0)
+
+        packet_size = self.queue.pop(0)
+        self.logger.log_queue_length(self, len(self.queue))
+
+        duration = packet_size * 8 / self.datarate
+        # transmit packet
+        packet = Packet(packet_size, duration)
+        if(packet.get_id() == 19):
+            pass
+        self.channel.start_transmission(self, packet)
+        # schedule end of transmission
+        end_tx = Event(self.sim.get_time() + duration, Events.END_TX, self,
+                       self, packet)
+        self.sim.schedule_event(end_tx)
+
+    def get_posx(self):
+        """
+        Returns x position
+        :returns: x position in meters
+        """
+        return self.x
+
+    def get_posy(self):
+        """
+        Returns y position
+        :returns: y position in meters
+        """
+        return self.y
--- node.py
+++ node.py
@@ -14,37 +14,24 @@
 # Copyright (C) 2016 Michele Segata <segata@ccs-labs.org>
 #                    Daniel Zozin <zdenial@gmx.com>
 
-from module import Module
-from fsm import FSM
-from distribution import Distribution
+
+from fsmNode import FSMNode
 from event import Event
 from events import Events
 from packet import Packet
 
 
-class Node(Module, FSM):
+class Node(FSMNode):
     """
     This class implements a node capable of communicating with other devices
     """
-
-    # transmission speed parameter (bits per second)
-    DATARATE = "datarate"
-    # queue size
-    QUEUE = "queue"
-    # inter-arrival distribution (seconds)
-    INTERARRIVAL = "interarrival"
-    # packet size distribution (bytes)
-    SIZE = "size"
-    # processing time distribution (seconds)
-    PROC_TIME = "processing"
-    # max packet size (bytes)
-    MAXSIZE = "maxsize"
 
     # list of possible states for this node
     IDLE = 0
     TX = 1
     RX = 2
     PROC = 3
+    SENSE = 4
 
     def __init__(self, config, channel, x, y):
         """
@@ -54,240 +41,160 @@ class Node(Module, FSM):
         :param x: x position
         :param y: y position
         """
-        Module.__init__(self)
+        FSMNode.__init__(self, config, channel, x, y)
 
         # Initialize the Finite State Machine with the transition table
-        FSM.__init__(self, Node.IDLE, {
-            (Node.IDLE, Events.PACKET_ARRIVAL): self.transmit_arrived,
+        self.set_transitions(Node.IDLE, {
+            # Try transmitting when a new packed is enqueued
+            # or when a packet in the air terminates
+            (Node.IDLE, Events.PACKET_ARRIVAL): self.try_transmitting,
+            (Node.SENSE, Events.END_RX): self.try_transmitting,
+
+            # Try receiving a packet in the air
             (Node.IDLE, Events.START_RX): self.try_receiving,
+
+            # Set the receiving packet as corrupted by another one
+            (Node.RX, Events.START_RX): self.corrupt_reception,
+
+            # The node is busy, do nothing with the newly enqueue packet
+            (Node.RX, Events.PACKET_ARRIVAL): self.stay,
+            (Node.PROC, Events.PACKET_ARRIVAL): self.stay,
+            (Node.TX, Events.PACKET_ARRIVAL): self.stay,
+            (Node.SENSE, Events.PACKET_ARRIVAL): self.stay,
+
+            # The node is busy, new packets detected in the air are dropped
+            (Node.PROC, Events.START_RX): self.drop_receiving,
+            (Node.TX, Events.START_RX): self.drop_receiving,
+            (Node.SENSE, Events.START_RX): self.drop_receiving,
+
+            # Detect the termination of a packet in the air and
+            # remain in the same state
             (Node.IDLE, Events.END_RX): self.end_packet,
-
-            (Node.RX, Events.PACKET_ARRIVAL): self.enqueue_arrived,
-            (Node.RX, Events.START_RX): self.set_corrupted,
-            (Node.RX, Events.END_RX): self.end_receiving,
-            (Node.RX, Events.RX_TIMEOUT): self.switch_to_proc,
-
-            (Node.PROC, Events.PACKET_ARRIVAL): self.enqueue_arrived,
-            (Node.PROC, Events.START_RX): self.set_corrupted,
             (Node.PROC, Events.END_RX): self.end_packet,
-            (Node.PROC, Events.END_PROC): self.resume_operations,
-
-            (Node.TX, Events.PACKET_ARRIVAL): self.enqueue_arrived,
-            (Node.TX, Events.START_RX): self.set_corrupted,
             (Node.TX, Events.END_RX): self.end_packet,
-            (Node.TX, Events.END_TX): self.switch_to_proc
+
+            # Start processing after transmission
+            (Node.TX, Events.END_TX): self.switch_to_proc,
+
+            # Start processing after reception
+            (Node.RX, Events.END_RX): self.end_receiving,
+
+            # Resume after processing terminates
+            (Node.PROC, Events.END_PROC): self.resume_operations
         })
 
-        # load configuration parameters
-        self.datarate = config.get_param(Node.DATARATE)
-        self.queue_size = config.get_param(Node.QUEUE)
-        self.interarrival = Distribution(config.get_param(Node.INTERARRIVAL))
-        self.size = Distribution(config.get_param(Node.SIZE))
-        self.proc_time = Distribution(config.get_param(Node.PROC_TIME))
-        self.maxsize = config.get_param(Node.MAXSIZE)
-        # queue of packets to be sent
-        self.queue = []
-        # current state
+        # current packet being received
+        self.current_rcv = None
 
-        self.logger.log_state(self, Node.IDLE)
-        # save position
-        self.x = x
-        self.y = y
-        # save channel
-        self.channel = channel
-        # current packet being either sent or received
-        self.current_pkt = None
         # count packets currently detected on channel
         self.packets_on_ch = 0
-        # timeout event used to avoid being stuck in the RX state
-        self.timeout_event = None
-        # timeout time for the rx timeout event. set as the time needed to
-        # transmit a packet of the maximum size plus a small amount of 10
-        # microseconds
-        self.timeout_time = self.maxsize * 8.0 / self.datarate + 10e-6
 
-    def initialize(self):
+    def try_transmitting(self, event=None):
         """
-        Initialization. Starts node operation by scheduling the first packet
+        If the channel is not free go in SENSE state,
+        otherwise start transmitting a packet (from the queue)
         """
-        self.schedule_next_arrival()
 
-    def handle_event(self, event):
-        FSM.handle_event(self, event)
+        if(not self.is_channel_free()):
+            return Node.SENSE
 
-    def schedule_next_arrival(self):
-        """
-        Schedules a new arrival event
-        """
-        # extract random value for next arrival
-        arrival = self.interarrival.get_value()
-
-        # draw packet size from the distribution
-        packet_size = self.size.get_value()
-
-        # generate an event setting this node as destination
-        event = Event(self.sim.get_time() + arrival, Events.PACKET_ARRIVAL,
-                      self, self, packet_size)
-        self.sim.schedule_event(event)
-
-    def transmit_arrived(self, event):
-        """
-        Handles a packet arrival
-        """
-        assert(len(self.queue) == 0)
-        self.enqueue_arrived(event)
-
-        return self.transmit_packet()
-
-    def enqueue_arrived(self, event):
-        packet_size = event.get_obj()
-        self.logger.log_arrival(self, event.get_obj())
-        if self.queue_size == 0 or len(self.queue) < self.queue_size:
-            # if queue size is infinite or there is still space
-            self.queue.append(packet_size)
-            self.logger.log_queue_length(self, len(self.queue))
-        else:
-            # if there is no space left, we drop the packet and log
-            self.logger.log_queue_drop(self, packet_size)
-
-        self.schedule_next_arrival()
+        self.transmit()
+        return Node.TX
 
     def try_receiving(self, event):
-        nextState = None
+        was_channel_free = self.is_channel_free()
 
-        new_packet = event.get_obj()
-
-        if self.is_channel_free():
-            # node is idle: it will try to receive this packet
-            assert(self.current_pkt is None)
-            new_packet.set_state(Packet.PKT_RECEIVING)
-            self.current_pkt = new_packet
-            nextState = Node.RX
-            # create and schedule the RX timeout
-            self.timeout_event = Event(self.sim.get_time() +
-                                       self.timeout_time, Events.RX_TIMEOUT,
-                                       self, self, None)
-            self.sim.schedule_event(self.timeout_event)
-            self.logger.log_state(self, Node.RX)
-        else:
-            # there is another signal in the air but we are IDLE. this
-            # happens if we start receiving a frame while transmitting
-            # another. when we are done with the transmission we assume we
-            # are not able to detect that there is another frame in the air
-            # (we are not doing carrier sensing). In this case we assume we
-            # are not able to detect the new one and set that to corrupted
-            new_packet.set_state(Packet.PKT_CORRUPTED)
-
-        # count this as currently being received
+        # count new packet in the channel
         self.add_detected_packet()
 
-        return nextState
-
-    def set_corrupted(self, event):
         new_packet = event.get_obj()
 
-        # node is either receiving or transmitting
-        if self.current_pkt is not None:
-            # the frame we are currently receiving is corrupted by a
-            # collision, if we have one
-            self.current_pkt.set_state(Packet.PKT_CORRUPTED)
+        # If there are other packets on the channel, they will interfere with
+        # this one, so set it to corrupted and don't try to receive it
+        if not was_channel_free:
+            new_packet.set_state(Packet.PKT_CORRUPTED)
+            return FSMNode.STAY
 
-        # the same holds for the new incoming packet. either if we are in
-        # the RX, TX, or PROC state, we won't be able to decode it
+        # Start receiving this packet
+        self.current_rcv = new_packet
+        self.current_rcv.set_state(Packet.PKT_RECEIVING)
+        return Node.RX
+
+    def corrupt_reception(self, event):
+        # count new packet in the channel
+        self.add_detected_packet()
+
+        # the packet we are currently receiving is corrupted by a
+        # collision with the new packet
+        self.current_rcv.set_state(Packet.PKT_CORRUPTED)
+
+        # Also the new packet is corrupted
+        new_packet = event.get_obj()
         new_packet.set_state(Packet.PKT_CORRUPTED)
 
-        # count this as currently being received
+        # Stay anyway in RX until the end event (RX Timeout event not needed)
+        return FSMNode.STAY
+
+    def drop_receiving(self, event):
+        # count new packet in the channel
         self.add_detected_packet()
 
+        # If the node is not in IDLE or RX, it is not able to decode a new
+        # packet so just ignore it and remain in same state
+        return FSMNode.STAY
+
     def end_receiving(self, event):
+        # count packet not in the channel anymore
         self.remove_detected_packet()
 
         packet = event.get_obj()
 
-        if packet.get_state() == Packet.PKT_RECEIVING:
-                # the packet is not in a corrupted state: we succesfully
-                # received it
+        # The packet is the one under reception
+        if packet.get_id() == self.current_rcv.get_id():
+            # the packet is not corrupted, so it is successfully received
+            if packet.get_state() == Packet.PKT_RECEIVING:
                 packet.set_state(Packet.PKT_RECEIVED)
-                # just to be sure: we can only correctly receive the packet we
-                # were trying to decode
-                assert(packet.get_id() == self.current_pkt.get_id())
 
-        # delete the timeout event
-        self.sim.cancel_event(self.timeout_event)
+            self.logger.log_packet(event.get_source(), self, packet)
 
+            # End reception and process packet (Even though it was corrupted)
+            return self.switch_to_proc(event)
+
+        # The packet is not the one under reception, so it should have
+        # already been marked as corrupted on its start event
+        assert(packet.get_state() == Packet.PKT_CORRUPTED)
+
+        # Just log it and stay in the same state
         self.logger.log_packet(event.get_source(), self, packet)
-
-        return self.switch_to_proc(event)
+        return FSMNode.STAY
 
     def end_packet(self, event):
+        # count packet not in the channel anymore and stay in the same state
         self.remove_detected_packet()
-        self.logger.log_packet(event.get_source(), self, event.get_obj())
+        return FSMNode.STAY
 
     def switch_to_proc(self, event):
         """
         Switches to the processing state and schedules the end_proc event
         """
+
         proc_time = self.proc_time.get_value()
         proc = Event(self.sim.get_time() + proc_time, Events.END_PROC, self,
                      self)
         self.sim.schedule_event(proc)
-
-        self.current_pkt = None
-
-        self.logger.log_state(self, Node.PROC)
         return Node.PROC
 
     def resume_operations(self, event):
         """
         Handles the end of the processing period, resuming operations
-        :param event: the END_PROC event
         """
 
         if len(self.queue) == 0:
-            self.logger.log_state(self, Node.IDLE)
             # resuming operations but nothing to transmit. back to IDLE
             return Node.IDLE
         else:
-            # there is a packet ready in the queue, trasmit it
-            return self.transmit_packet()
-
-    def transmit_packet(self):
-        """
-        Sends and enqueued packet
-        """
-
-        assert(len(self.queue) > 0)
-
-        packet_size = self.queue.pop(0)
-        self.logger.log_queue_length(self, len(self.queue))
-
-        assert(self.current_pkt is None)
-        duration = packet_size * 8 / self.datarate
-        # transmit packet
-        packet = Packet(packet_size, duration)
-        self.channel.start_transmission(self, packet)
-        # schedule end of transmission
-        end_tx = Event(self.sim.get_time() + duration, Events.END_TX, self,
-                       self, packet)
-        self.sim.schedule_event(end_tx)
-        self.current_pkt = packet
-
-        self.logger.log_state(self, Node.TX)
-        return Node.TX
-
-    def get_posx(self):
-        """
-        Returns x position
-        :returns: x position in meters
-        """
-        return self.x
-
-    def get_posy(self):
-        """
-        Returns y position
-        :returns: y position in meters
-        """
-        return self.y
+            # there is a packet ready in the queue, try transmitting it
+            return self.try_transmitting()
 
     def is_channel_free(self):
         return self.packets_on_ch == 0
