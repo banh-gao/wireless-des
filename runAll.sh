12313fbbce7f08184381035e74f8559b98d19e46
Daniel Zozin <zdenial@gmx.com>
2016-12-19 20:39:14 +0100

    Parallelize with GNU parallel

--- runAll.sh
+++ runAll.sh
@@ -1,52 +1,31 @@
 #!/bin/bash
 
+set -e
+
 RES_DIR=/tmp
 OUT_DIR="$( pwd )"
 
-PIDs=()
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Setup proper cleanup
-cleanup() {
-    for pid in "${PIDs[@]}"; do
-        kill $pid 2> /dev/null
-    done
-    echo "Simulations abnormally halted!"
-    exit 1
-}
-
-trap cleanup HUP INT QUIT KILL PIPE TERM
 
 if [ $# -ne 2 ]; then
     echo "usage: $0 confFile maxSimulation"
     exit 0
 fi
 
-max=$2
 
-for i in $(seq 0 $max); do
-    echo "Starting simulation #$i ..."
-    $SRC_DIR/main.py -c $1 -r $i 1> "$RES_DIR/sim_$i.log" &
-    PIDs[$i]=$!
-done
+# Setup proper cleanup
+cleanup() {
+    echo "Simulations abnormally halted!"
+    echo "See simulations data in $RES_DIR"
+    exit 1
+}
+trap cleanup HUP INT QUIT KILL PIPE TERM
 
-i=0
-for pid in "${PIDs[@]}"; do
-    wait $pid
-
-    # Halt all if simulation failed
-    if [ $? -ne 0 ]; then
-        echo "SIMULATION FAILURE DETECTED!"
-        cleanup
-        exit 1
-    fi
-
-    echo "Simulation $((i + 1)) of $((max + 1)) ended"
-    i=$((i + 1))
-done
+echo "Running simulations from 0 to $2 ..."
+parallel --progress "$SRC_DIR/main.py -c $1 -r {} 1> $RES_DIR/sim_{}.log" ::: $(seq 0 $2)
 
 echo "Compressing results ..."
-Rscript --vanilla $SRC_DIR/compress.R $RES_DIR $OUT_DIR 1> /dev/null
+Rscript --vanilla $SRC_DIR/compress.R $RES_DIR $OUT_DIR
 echo "Cleaning up temp files ..."
 rm $RES_DIR/output_*.csv
 
