716f94d6a38835ddaed11fd6ab1d566f4071f1b1
Daniel Zozin <zdenial@gmx.com>
2016-12-19 09:57:53 +0100

    Run simulations as background processes to speedup on multi-cores

--- runAll.sh
+++ runAll.sh
@@ -1,7 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-for i in $(seq 0 $2); do
-    echo "Running simulation $i of $2 ..."
-    ./main.py -c $1 -r $i
-    echo ""
+PIDs=()
+
+# Setup proper cleanup
+cleanup() {
+    for pid in "${PIDs[@]}"; do
+        kill $pid
+    done
+    echo""
+    echo "Simulations abnormally halted!"
+    exit 1
+}
+
+trap cleanup HUP INT QUIT KILL PIPE TERM
+
+max=$2
+
+echo "Running $((max + 1)) simulations (from #0 to #$max) ..."
+for i in $(seq 0 $max); do
+    ./main.py -c $1 -r $i > "sim_$i.log" &
+    PIDs[$i]=$!
 done
+
+i=0
+for pid in "${PIDs[@]}"; do
+    wait $pid
+    remaining=$((max - $i))
+    if [ $remaining -ne 0 ]; then
+        echo "Simulation #$i ended. $remaining simulations still running ..."
+    else
+        echo "Simulation #$i ended."
+    fi
+    i=$((i+1))
+done
+
+echo "All $((max + 1)) simulations successfully ended."
